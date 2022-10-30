4d7b7f3da8d2841c9a9a1e7f7c38a854d80363e4
Msksgm <msksgm3124@gmail.com>
2022-10-30 15:47:33 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlinspringbootmockmvcjsonassertregex/KotlinSpringBootMockmvcJsonassertRegexApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringbootmockmvcjsonassertregex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootMockmvcJsonassertRegexApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootMockmvcJsonassertRegexApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinspringbootmockmvcjsonassertregex/KotlinSpringBootMockmvcJsonassertRegexApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringbootmockmvcjsonassertregex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootMockmvcJsonassertRegexApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
